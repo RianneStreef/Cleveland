166e1e2bb3bd6cba7ae86feeb9ef80b6f7571237
RianneStreef <riannestreef@gmail.com>
2021-02-17 16:07:04 +0100

    index and categories done

--- src/script.js
+++ src/script.js
@@ -1,26 +1,42 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+console.log('script running');
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+const saveMsg = document.getElementById('save-message');
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+addBtn.style.display = 'none';
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('eventlistener activated')
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+
+    if (addBtn.style.display == "block") {
+      saveMsg.style.display = "none";
+    };  
+
+});
+
+
+
